ddc081d42d4be3195018f4411f6172eb8772ef25
Curry30Messi <2316072618@qq.com>
2024-10-30 20:22:39 +0800

    success

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 cd /scratch/project_2003204/zrx/PORPOISE/ || exit
 export PATH="/scratch/project_2003204/zrx/anaconda3/bin:$PATH"
 source activate base
-srun -N 1 -n 1 -c 6 -t 00:15:00 --mem-per-cpu=16G --gres=gpu:a100:1 -p gputest --account=project_2003204 python main.py --which_splits 5foldcv --split_dir tcga_luad --mode pathomic --reg_type pathomic --model_type porpoise_mmf --data_root_dir /scratch/project_2003204/zrx/results/LUAD/feature/
+srun -N 1 -n 1 -c 6 -t 31:55:00 --mem-per-cpu=16G --gres=gpu:a100:1 -p gpusmall --account=project_2003204 python main.py --which_splits 5foldcv --split_dir tcga_luad --mode pathomic --reg_type pathomic --model_type porpoise_mmf --data_root_dir /scratch/project_2003204/zrx/results/LUAD/feature/
