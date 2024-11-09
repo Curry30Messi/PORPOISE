6b760bdb0904f87df335ad0c3393cdbc7504f69d
Curry30Messi <2316072618@qq.com>
2024-11-09 11:39:57 +0800

    5fold cv

--- all.sh
+++ all.sh
@@ -1,4 +1,2 @@
-srun -N 1 -n 1 -c 6 -t 31:55:00 --mem-per-cpu=16G --gres=gpu:a100:1 -p gpusmall --account=project_2003204 bash s1.sh && \
-srun -N 1 -n 1 -c 6 -t 31:55:00 --mem-per-cpu=16G --gres=gpu:a100:1 -p gpusmall --account=project_2003204 bash s2.sh && \
 srun -N 1 -n 1 -c 6 -t 31:55:00 --mem-per-cpu=16G --gres=gpu:a100:1 -p gpusmall --account=project_2003204 bash s3.sh && \
 srun -N 1 -n 1 -c 6 -t 31:55:00 --mem-per-cpu=16G --gres=gpu:a100:1 -p gpusmall --account=project_2003204 bash s4.sh
